b6c943e5c06b9779243fec4e76020eaeb9ca309f
Joao Marcos <joaoobx@gmail.com>
2021-01-08 09:23:07 -0300

    Modificação teste

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Novo app next</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
